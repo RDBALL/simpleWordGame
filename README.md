a78e49f41f47140e760dfd2f4d1f46e7a85868fc
Robert Ball <bobball89@gmail.com>
2022-12-26 18:54:52 +0000

    app and README upload

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # simpleWordGame
+
+**Author: Robert Ball**
+
 Python Script that takes user input and checks against 'correct' answers to guide the player home
+
+## Example
+
+![Script output](./assets/output.jpg)
+
+When a user runs this script they are greeted by this initial screen that asks them to find the way home for their character.
+
+There are three prompts in total that the user will need to answer correctly in order to win the game.
+
+A simple way to think about the process of incorrect versus correct answers is to view the prompts as a flow chart
+
+```md
+    Prompt 1
+        |
+   right wrong
+   /        \
+prompt 2     end
+  |    \
+right wrong
+  |       \
+prompt 3   end
+  |    \
+right wrong
+  |     |
+ end   end
+```
+
+The logic that determines if the game should continue with the next prompt or to end the game is a series of nested `if` and `elif` statements.
+
+User input is converted to lower case and is checked against a variable that is assigned the "correct" answer which can only be the strings `"left"` or `"right"`.
+
+If the user input equals variable that is assigned to the prompt then the script will progress to the next prompt. If the user input does not equal the variable assigned to the prompt then the script will end.
+
+*[Ascii art attribution](https://ascii.co.uk/art/home)*
---
+++ app.py
@@ -0,0 +1,37 @@
+print('''
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+    88                                                     
+    88                                                     
+    88                                                     
+    88,dPPYba,   ,adPPYba,  88,dPYba,,adPYba,   ,adPPYba,  
+    88P'    "8a a8"     "8a 88P'   "88"    "8a a8P_____88  
+    88       88 8b       d8 88      88      88 8PP"""""""  
+    88       88 "8a,   ,a8" 88      88      88 "8b,   ,aa  
+    88       88  `"YbbdP"'  88      88      88  `"Ybbd8"'  
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+''')
+
+print("Please help me find my way home")
+
+stepOne = input("Do I turn left or right?\n").lower()
+
+stepOneCorrect = 'right'
+stepTwoCorrect = 'left'
+stepThreeCorrect = 'left'
+
+if stepOne == stepOneCorrect:
+  print("You turn right and see an intersection that you walk towards")
+  stepTwo = input("Now I am at an intersection, should I cross the street on the right or the left?\n").lower()
+  if stepTwo == stepTwoCorrect:
+    print("You cross the intersection on the left")
+    stepThree = input ("I crossed the street and there are two doors. Do I go into the left door or the right door?\n").lower()
+    if stepThree == stepThreeCorrect:
+      print("You go into the door on the left and find yourself back at your home")
+    elif stepThree != stepThreeCorrect:
+      print("You go into the door on the right and realize this is not your home")
+  elif stepTwo != stepTwoCorrect:
+    print("You cross the intersection on the right and are hit by a car")
+elif stepOne != stepOneCorrect:
+  print("You turn left and fall in a hole")
